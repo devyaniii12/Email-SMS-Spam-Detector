b2f57ad57cb6c306b523656f894ae5905b28dff3
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-08-10 22:59:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+# Download NLTK resources
+nltk.download('punkt')
+nltk.download('stopwords')
+
 ps = PorterStemmer()
 
 
